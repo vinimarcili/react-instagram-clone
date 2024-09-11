6a0c05e39d0133cb4015b9e9b3bca4aa5b1ff892
Vinícius Marcili <vinipetter@gmail.com>
2024-09-11 19:41:54 -0300

    Layout base

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import "./App.css"
 import HomePage from "./pages/Home/Home.page"
 import NotFoundPage from "./pages/NotFound/NotFound.page"
 import UserPage from "./pages/User/User.page"
 import PostPage from "./pages/Post/Post.page"
+import Header from "./components/Header/Header"
+import Footer from "./components/Footer/Footer"
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/:username" element={<UserPage />} />
-        <Route path="/p/:id" element={<PostPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <Header />
+      <main className="mx-auto p-4 w-full h-full">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/:username" element={<UserPage />} />
+          <Route path="/p/:id" element={<PostPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </main>
+      <Footer />
     </BrowserRouter>
   )
 }
