8bcb411aa34317bd95276b4521b151d72b888ac4
Vinícius Marcili <vinipetter@gmail.com>
2024-09-11 20:59:09 -0300

    Refactor Instagram service to use API types, add type annotations, and include new image URLs

--- src/services/instagram.service.ts
+++ src/services/instagram.service.ts
@@ -2,6 +2,8 @@ import { InstagramResponse } from "../types/api.type"
 import { Post } from "../types/post.type"
 
 const API_URL = './public/data.json'
+const USER_IMAGE_URL = 'https://robohash.org/'
+const FEED_IMAGE_URL = 'https://picsum.photos/id/'
 
 export const fetchInstagram = async (): Promise<InstagramResponse> => {
   const response = await fetch(API_URL)
@@ -21,4 +23,12 @@ export const getPost = async (id: string): Promise<Post | null> => {
 export const getPostsByUser = async (username: string): Promise<Post[]> => {
   const response = await fetchInstagram()
   return response?.data?.filter((post) => post.user.username === username) ?? []
+}
+
+export const getUserPhotoURL = async (username: string): Promise<string> => {
+  return `${USER_IMAGE_URL}${username}`
+}
+
+export const getFeedPhotoURL = async (id: string, size = 500): Promise<string> => {
+  return `${FEED_IMAGE_URL}${id}/${size}/${size}`
 }
